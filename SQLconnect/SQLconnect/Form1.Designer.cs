96c0ef57c0a7820cfb9a0f00ba33a840725cfb95
ticross18 <63727030+ticross18@users.noreply.github.com>
2021-05-14 11:57:30 -0400

    Add files via upload

---
+++ SQLconnect/SQLconnect/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SQLconnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvNames = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNames)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(166, 74);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(395, 20);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(614, 74);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(85, 26);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Find";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvNames
+            // 
+            this.dgvNames.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNames.Location = new System.Drawing.Point(69, 126);
+            this.dgvNames.Name = "dgvNames";
+            this.dgvNames.Size = new System.Drawing.Size(659, 322);
+            this.dgvNames.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(183, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search Employee by Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 503);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvNames);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNames)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvNames;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SQLconnect/SQLconnect/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLconnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+
+
+           
+
+            string connectionString = "Data Source=DESKTOP-M7U7OLU;Initial Catalog=sqlWinforms;User ID=Tiffany;Password=abc123";
+            SqlConnection cnn = new SqlConnection(connectionString);
+            cnn.Open();
+
+            //string sql = "SELECT * FROM employees";
+            string sql = "SELECT empID as ID, empName as Name, empAddress as Address, empPhone as Phone, empEmail as Email, empBirthdate as Birthdate from employees  WHERE empName LIKE '" + txtSearch.Text + "%'";
+            SqlCommand command = new SqlCommand(sql, cnn);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, cnn);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dgvNames.ReadOnly = true;
+            dgvNames.AllowUserToAddRows = false;
+            dgvNames.DataSource = ds.Tables[0];
+
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                MessageBox.Show("No results found");
+            }
+
+            cnn.Close();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
